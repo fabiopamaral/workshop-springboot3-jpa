e7f943c87bf876f72eac8f3c958cf870bebfca02
fabiopamaral <fabio.pamaral@outlook.com>
2023-10-02 13:06:27 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,45 +4,53 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
-//@Component Registra a classe como componente do Spring, podendo ser automaticamente injetado pelo @Autowired
-@Service //Igual ao @Component, porém mais especifico
+@Service
 public class UserService {
-    
-    @Autowired
-    private UserRepository repository;
 
-    public List<User> findAll() {
-        return repository.findAll(); //Serviço manda o Repositório buscar os dados
-    }
+	@Autowired
+	private UserRepository repository;
 
-    public User findById(Long id) {
-        Optional<User> obj = repository.findById(id);
-        return obj.orElseThrow(() -> new ResourceNotFoundException(id)); //Tenta dar o get. Se não conseguir lança a exceção
-    }
+	public List<User> findAll() {
+		return repository.findAll();
+	}
 
-    public User insert(User obj) {
-        return repository.save(obj);
-    }
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
 
-    public void delete(Long id) {
-        repository.deleteById(id);
-    }
-
-    public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id); //Somente prepara o objeto monitorado
-        updateData(entity, obj);
-        return repository.save(entity);
-    }
-
-    private void updateData(User entity, User obj) {
-        entity.setName(obj.getName());
-        entity.setEmail(obj.getEmail());
-        entity.setPhone(obj.getPassword());
-    }
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
